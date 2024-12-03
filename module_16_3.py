04963df34a48fee3f4dec16d7805d1e2253d2dc6
Feo255 <dyngafiodor@gmail.com>
2024-12-03 18:40:34 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -16,19 +16,25 @@ async def get_all_users():
     return users
 
 @app.post("/user/{username}/{age}")
-async def create_user(username: str, age: int) -> str:
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return {f"User {user_id} is registered"}
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, username: str, age: int):
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")],
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]):
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return {f"The user {user_id} is updated"}
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str):
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]):
     users.pop(user_id)
     return {f"The user {user_id} is deleted"}
     raise HTTPException(status_code=404, detail="User not found")
