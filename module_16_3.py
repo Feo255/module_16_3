1401b8fb257beda0ab65b054ac4ff231d5212b17
Feo255 <dyngafiodor@gmail.com>
2024-12-06 12:20:11 +0300

    Update module_16_3.py

--- module_16_3.py
+++ module_16_3.py
@@ -21,7 +21,8 @@ async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
         age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return {f"User {user_id} is registered"}
+    resp=f"User {user_id} is registered"
+    return resp
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")],
@@ -35,9 +36,6 @@ async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description="En
 @app.delete("/user/{user_id}")
 async def delete_user(
         user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]):
-    users.pop(user_id)
+    users.pop(str(user_id))
     return {f"The user {user_id} is deleted"}
     raise HTTPException(status_code=404, detail="User not found")
-
-
-
